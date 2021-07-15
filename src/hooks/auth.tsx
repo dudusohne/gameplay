36b14bbe36eb61fa051bfc42b9a6dcd338b2bae6
Eduardo Sohne <dudu.hit@gmail.com>
2021-07-15 13:33:00 -0300

    update 15-07

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -6,6 +6,16 @@ import React,
     useState
 } from 'react';
 
+import * as AuthSession from 'expo-auth-session';
+import {
+    SCOPE,
+    CLIENT_ID,
+    CDN_IMAGE,
+    REDIRECT_URI,
+    RESPONSE_TYPE
+} from '../configs';
+import { api } from '../services/api';
+
 type User = {
     id: string;
     username: string;
@@ -17,6 +27,7 @@ type User = {
 
 type AuthContextData = {
     user: User;
+    signIn: () => Promise<void>;
 }
 
 type AuthProviderProps = {
@@ -26,10 +37,26 @@ export const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>({} as User);
+    const [loading, setLoading] = useState(false);
+
+    async function signIn() {
+        try {
+            setLoading(true);
+
+            const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
+
+            const response = AuthSession.startAsync({ authUrl });
+            console.log(response);
+
+        } catch {
+            throw new Error('Não foi possível autenticar');
+        }
+    }
 
     return (
         <AuthContext.Provider value={{
-            user
+            user,
+            signIn
         }}>
             {children}
         </AuthContext.Provider>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 
-import { View, Text, Image } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, Alert } from 'react-native';
 import { ButtonIcon } from '../../components/ButtonIcon';
 import { Background } from '../../components/Background';
 import { useAuth } from '../../hooks/auth';
 
 import IllustrationImg from '../../assets/illustration.png';
+
 import { styles } from './styles';
 
 export function SignIn() {
 
-  const navigation = useNavigation();
-  const { user } = useAuth();
+  const { user, signIn } = useAuth();
 
-  function handleSignIn() {
-    navigation.navigate('Home');
+  async function handleSignIn() {
+    try {
+      await signIn();
+    } catch (error) {
+      Alert.alert(error)
+    }
   }
 
   return (
