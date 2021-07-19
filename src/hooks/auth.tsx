6290109337da85fa84960d2bd17e053f821b832b
Eduardo Sohne <dudu.hit@gmail.com>
2021-07-19 10:21:23 -0300

    Update auth.tsx

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -46,7 +46,7 @@ function AuthProvider({ children }: AuthProviderProps) {
             const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`
 
             const response = AuthSession.startAsync({ authUrl });
-            console.log(response);
+            // console.log(response);
 
         } catch {
             throw new Error('Não foi possível autenticar');
